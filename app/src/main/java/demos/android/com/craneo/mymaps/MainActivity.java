13faa8f1214cb6b878ccb4068f526e0d92287a60
Andres Gonzalez Paez <andres.gonzalez.paez1@gmail.com>
2016-10-16 20:19:53 -0400

    Start the exercise with Geocode

--- app/src/main/java/demos/android/com/craneo/mymaps/MainActivity.java
+++ app/src/main/java/demos/android/com/craneo/mymaps/MainActivity.java
@@ -1,5 +1,6 @@
 package demos.android.com.craneo.mymaps;
 
+import android.graphics.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,14 +9,26 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
+    private static final float ZOOM_VALUE = 15;
     GoogleMap mMap;
     private static final int ERROR_DIALOG_REQUEST = 901;
+    private static final double
+                        SEATTLE_LAT = 47.60621,
+                        SEATTLE_LNG = -122.33207,
+                        SYDNEY_LAT = -33.867487,
+                        SYDNEY_LNG = 150.20699,
+                        NEWYORK_LAT = 40.714354,
+                        NEWYORK_LNG = -74.005973;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         if(servicesOk()){
             setContentView(R.layout.activity_map);
+            initMap();
             Toast.makeText(this, "Ready to map!", Toast.LENGTH_SHORT).show();
         }else{
             setContentView(R.layout.activity_main);
@@ -48,9 +62,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    private void initMap() {
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
 
     public boolean servicesOk(){
         GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
@@ -69,6 +89,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-//        setUpMap();
+        gotoLocation(SEATTLE_LAT, SEATTLE_LNG, ZOOM_VALUE) ;
+    }
+
+    private void gotoLocation(double latitud, double longitud, float zoom){
+        LatLng latLng = new LatLng(latitud, longitud);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
+        mMap.moveCamera(update);
     }
 }
